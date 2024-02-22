ee57a5a0092119354a5e03dceee6adea660e7a73
dilshanzz <dilshanwijewardhana999@gmail.com>
2024-02-22 21:43:56 +0530

    registration Ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 
 @NgModule({
-    imports: [],
+    imports: [HttpClientModule,FormsModule],
     exports: []
 })
 
--- src/app/common/sr/sr.component.ts
+++ src/app/common/sr/sr.component.ts
@@ -1,12 +1,33 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sr',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule],
   templateUrl: './sr.component.html',
   styleUrl: './sr.component.css'
 })
 export class SrComponent {
 
+  private http;
+
+  constructor(private httpClient:HttpClient){
+    this.http = httpClient;
+  }
+
+  public student = {
+    firstName: '',
+    lastName: '',
+    contact:'',
+  };
+
+  createStudent(){
+    this.http
+    .post("http://localhost:8080/student/add",this.student)
+    .subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
